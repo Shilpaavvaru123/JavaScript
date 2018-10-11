7295afe2717b429516e92f1edddc0236cfd4df79
Shilpaavvaru123 <avvarushilpa3@gmail.com>
2018-10-11 12:35:22 -0600

    hiii

---
+++ first.js
@@ -0,0 +1,95 @@
+/ javascript supports 3 types of DataTypes /
+
+string => "shilpa"; "Hello World" ;"200"; "true"
+number => 100
+boolean => true/false
+
+
+/ Variables //
+/*Char | .net, java are the key words
+int | 
+bool  | */
+
+/* JavaScript  Variables*/
+var a ="Shilpa"
+var b ="100"
+var c = true
+var d = 300
+
+/*Es5(Ecma Script)  ----- Java Script Version*/
+
+1+1
+2
+var a ="shilpa"
+undefined
+a
+"shilpa"
+typeof(a)  --- inbuilt operator
+"string"
+var b = 100
+undefined
+b
+100
+typeof(b)
+"number"
+var c = true
+undefined
+c
+true
+typeof(c)
+"boolean"
+var d = "20"
+undefined
+typeof(d)
+"string"
+
+var x =4 --- / Value is getting  override,  we can override and redeclare in the Var/
+undefined
+x
+4
+var x =6
+undefined
+
+let y = "shilpa"
+undefined
+y
+"shilpa"
+let y ="me"
+/*VM354:1 Uncaught SyntaxError: Identifier 'y' has already been declared
+it cannot override the value*/
+
+/* Constant
+No ReDeclare , No Override*/
+const s ="shi"
+undefined
+s
+"shi"
+const s ="lpa"
+/* Uncaught SyntaxError: Identifier 's' has already been declared */
+
+/ Operators /
+
+l = 10
+10
+k = 20
+20
+l+k
+30
+l/k
+0.5
+l*k
+200
+4%2
+0
+6%3
+0
+10%3
+1
+12%3
+0
+
+
+  
+
+
+
---
+++ second.js
@@ -0,0 +1,81 @@
+/* + for number =add
+ + for string = concatenate
+ string will work with only + operator */
+
+ var r = "hello"
+undefined
+var y = "world"
+undefined
+r+y
+"helloworld"
+a= 10
+10
+b = 20
+20
+
+r*y
+NaN
+r-y
+NaN 
+/ (Not a Number) /
+
+
+/* can we add Number + String (interview question)
+number + number will add
+number with string will concate
+ ex:*/
+ 10 + 10 + "10"
+"2010"
+10 + "helloworld"
+"10helloworld"
+"20" + 30
+"2030"
+
+10" + 20 + 10
+"102010"
+
+10 + "20" +40
+"102040"
+
+/* Boolen operater
+ True = 1
+ false = 0
+
+ boolen + number ccan add */
+ true + true
+2
+true -1
+0
+true - 1
+0
+true -0
+1
+false + false
+0
+false + true
+1
+false -1
+-1
+false + 1
+1
+
+var txt = "aanmjhgfdsfghjk"
+undefined
+txt
+"aanmjhgfdsfghjk"
+txt.lenght
+undefined
+txt.length
+15
+txt.toUpperCase()
+"AANMJHGFDSFGHJK"
+txt.toLowerCase()
+"aanmjhgfdsfghjk"
+
+var a = txt.toLowerCase()
+undefined
+a
+"aanmjhgfdsfghjk"
+
+
+
